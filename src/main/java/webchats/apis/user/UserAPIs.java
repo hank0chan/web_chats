09cf18a3e6270bf4b790d7c4b14c8d30645898a3
hank0chan <hicth_chan@163.com>
2016-09-28 13:12:58 +0800

    提交毕业设计改进版本模型

---
+++ src/main/java/webchats/apis/user/UserAPIs.java
@@ -0,0 +1,12 @@
+package webchats.apis.user;
+
+/**
+ * 响应用户个人信息相关的API接口
+ * @author Hank_  
+ * <p>Email:hicth_chan@163.com</p>
+ * @version 创建时间: 28 Sep 201610:29:08
+ * <p>类说明:
+ */
+public class UserAPIs {
+
+}
